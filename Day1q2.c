d99ae635926e918ce9148a1b739f086039e43641
anushika321 <anushikachoudhary392@gmail.com>
2025-08-28 09:59:50 +0530

    Add files via upload

---
+++ Day1q2.c
@@ -0,0 +1,33 @@
+//Q2 (User Inputs, Operations & Output)
+//Write a program to input two numbers and display their sum, difference, product, and quotient.
+
+#include <stdio.h>
+int main()
+{
+    //S-Sum, D-Difference, P-Product, Q-Quotient
+    int x, y, S, D, P, Q;
+    printf("Enter the first number: \n");
+    scanf("%d", &x);
+    printf("Enter the second number: \n");
+    scanf("%d", &y);
+
+    S = x + y;
+    D = x - y;
+    P = x * y;
+    
+    if(y==0){
+    printf("Division is undefined\n");
+    }
+    else{
+    Q=x/y;
+    printf("Quotient of the numbers: %d\n", Q);
+    }
+
+
+    printf("Sum of the numbers: %d\n", S);
+    printf("Difference of the numbers: %d\n", D);
+    printf("Products of the numbers: %d\n", P);
+    
+    
+    return 0;
+}
