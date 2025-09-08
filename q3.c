b6c2cc42e6e5faaf4c590c8cdce81a687e0ff5d1
anushika321 <anushikachoudhary392@gmail.com>
2025-09-09 00:57:50 +0530

    Add files via upload

---
+++ q3.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    int j , i , n ;
+    for (i = 0 ; j = n ; i++){
+
+        for (j = 0 ; j = n ; j++){
+
+            printf("* ");
+
+        }
+        printf("\n");
+
+    
+    }
+    return 0;
+}
