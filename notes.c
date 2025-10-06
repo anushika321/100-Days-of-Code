23ee6a1854b939ceb0252bd6f1209f06506fa6aa
anushika321 <anushikachoudhary392@gmail.com>
2025-10-06 23:51:49 +0530

    Add files via upload

---
+++ notes.c
@@ -0,0 +1,8 @@
+void main()
+{
+    int a=2 , b;
+    b=a++ + ++a +a+ + ++a;
+    printf("%d",b);
+
+    
+}
---
+++ notes1.c
@@ -0,0 +1,6 @@
+void main()
+{
+    int a=1,b;
+    b= a++ + ++a +a++ + ++a +a++ + ++a;
+    printf("%d,%d",a,b);
+}
