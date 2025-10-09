251e91d519310713bf7a21495b59fd37d9bd02fc
anushika321 <anushikachoudhary392@gmail.com>
2025-10-09 23:34:19 +0530

    Add files via upload

---
+++ notes5.c
@@ -0,0 +1,14 @@
+void main()
+{
+    int i;
+    i=2;
+    switch(i)
+    {
+        case1 : printf("A");
+        case2 : printf("B");
+        case3 : printf("C");
+        case4 : printf("D");
+        default : printf("no");
+    }
+
+}
